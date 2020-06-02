53b5effc3764caf27629558b7a2795c5bdb5fcf2
Francis Rayos del Sol <francis.rayos@outlook.com>
2020-06-01 21:02:43 -0400

    created redshift cluster

--- vars.tf
+++ vars.tf
@@ -2,3 +2,48 @@ variable "aws_region" {
   description = "specify provider's region"
   default     = "us-east-2"
 }
+variable "vpc_cidr" {
+  description = "vpc range"
+  default     = "10.0.0.0/16"
+}
+
+variable "redshift_subnet_cidr_1" {
+  description = "redshift subnet 1"
+  default     = "10.0.1.0/24"
+}
+
+variable "redshift_subnet_cidr_2" {
+  description = "redshift subnet 2"
+  default     = "10.0.2.0/24"
+}
+
+# Define the Redshift Cluster
+variable "rs_cluster_identifier" {
+  description = ""
+  default     = "sample-cluster"
+}
+
+variable "rs_database_name" {
+  description = ""
+  default     = "samplecluster"
+}
+
+variable "rs_master_username" {
+  description = ""
+  default     = "sampleuser"
+}
+
+variable "rs_master_pass" {
+  description = ""
+  default     = "<PASSWORD>"
+}
+
+variable "rs_nodetype" {
+  description = ""
+  default     = "dc2.large"
+}
+
+variable "rs_cluster_type" {
+  description = ""
+  default     = "single-node"
+}
